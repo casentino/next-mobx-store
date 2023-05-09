db31059a80007b5edde37b0bc2c53b837e356d50
casentino <opterk@gmail.com>
2023-05-09 22:43:34 +0900

    Hydration 에 필요한 타입정의

---
+++ packages/next-mobx-store-type/HydrationUtilType.ts
@@ -0,0 +1,16 @@
+import { HydrationStore } from './HydrationType';
+import { IHydrationStore } from './IHydrationStore';
+
+export type SerailizedStore<Store extends IHydrationStore> = HydrationStore<Store>;
+
+export type DesrializedStore<Store extends IHydrationStore, HStore extends HydrationStore> = Partial<{
+	[K in keyof HStore]: K extends keyof Store ? Store[K] : HStore[K];
+}>;
+
+export type EntriesType<Store extends IHydrationStore> = [keyof Store, StoreValues<Store>];
+
+type StoreValues<
+	Store extends IHydrationStore,
+	S extends HydrationStore<Store> = HydrationStore<Store>,
+	Key extends keyof S = keyof S
+> = S[Key];
