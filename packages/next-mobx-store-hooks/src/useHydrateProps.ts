2158c2eaad4808c43ef2b7b9d7f1ae98b4027f7a
Kyeongoh-Ki <56759587+casentino@users.noreply.github.com>
2023-05-15 20:20:37 +0900

    hydration 훅 작성 (#7)
    * useRootStore 커스텀 훅
    
    * page 에서 만 사용될 스토어를 hydration 할 수 있도록 훅 작성

---
+++ packages/next-mobx-store-hooks/src/useHydrateProps.ts
@@ -0,0 +1,36 @@
+import { initializeRootStore } from '@next-mobx-store/core';
+import { HydrationDataType, IRootStore } from '@next-mobx-store/type';
+import { AppProps } from 'next/app';
+type UseHydratePropsReturn<Props> = {
+	store: IRootStore;
+	props: Props;
+};
+export default function useHydrateProps<
+	P extends Omit<
+		AppProps<{
+			hydrationData?: HydrationDataType & {
+				localPageProps?: Partial<LocalPageStores>;
+			};
+		}>,
+		'Component'
+	>,
+	LocalPageStores extends Record<string, object>
+>(
+	props: P
+): UseHydratePropsReturn<
+	Omit<P, 'pageProps'> & {
+		localPageProps?: Partial<LocalPageStores>;
+	}
+> {
+	const { pageProps, ...other } = props;
+	const { hydrationData } = pageProps;
+	let localPageProps: Partial<LocalPageStores> = {};
+	let hydrationDataType: HydrationDataType = {};
+	if (hydrationData && hydrationData.localPageProps) {
+		const { localPageProps: localPagePropsPartial, ...hydrationStores } = hydrationData;
+		localPageProps = localPagePropsPartial;
+		hydrationDataType = hydrationStores;
+	}
+
+	return { store: initializeRootStore(hydrationDataType), props: { localPageProps, ...other } };
+}
---
+++ packages/next-mobx-store-hooks/src/useLocalHydrateStore.ts
@@ -0,0 +1,12 @@
+import { deserializationStore } from '@next-mobx-store/core';
+import { LocalHydrateStores, LocalPageProps } from '@next-mobx-store/type';
+
+export default function useLocalHydrateStores<P extends LocalPageProps>(stores: LocalHydrateStores, props: P) {
+	const { localPageStores } = props;
+	if (!localPageStores) return;
+	const storeEntries = Object.entries(stores);
+	storeEntries.forEach(([storeName, store]) => {
+		if (!localPageStores[storeName]) return;
+		store.hydrate(deserializationStore(store, localPageStores[storeName]));
+	});
+}
