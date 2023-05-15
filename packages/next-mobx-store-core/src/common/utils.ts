64e19e23351fd858427f14970bfa85cf982968e6
casentino <opterk@gmail.com>
2023-05-15 20:24:41 +0900

    hydrate 함수 존재하는지 타입체크하는 함수 작성

--- packages/next-mobx-store-core/src/common/utils.ts
+++ packages/next-mobx-store-core/src/common/utils.ts
@@ -1,7 +1,16 @@
+import { IHydrationStore } from '@next-mobx-store/type';
+
 export const getIsServer = () => typeof window === 'undefined';
 
 export function isNullable(value: unknown) {
 	if (value === undefined || value === null || Number.isNaN(value)) {
+		return true;
+	}
+	return false;
+}
+
+export function hasHydrate(store: object): store is IHydrationStore {
+	if ('hydrate' in store && store.hydrate instanceof Function) {
 		return true;
 	}
 	return false;
