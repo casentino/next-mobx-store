7ab107e551e790fc7d32754c415639c16b155fa7
casentino <opterk@gmail.com>
2023-06-07 07:25:24 +0900

    tsup 빌드 설정 수정

--- packages/next-mobx-store-core/src/__test__/mocks/RootStore.ts
+++ packages/next-mobx-store-core/src/__test__/mocks/RootStore.ts
@@ -1,9 +1,8 @@
-import { HydrationDataType, IHydrationStore, IRootStore } from '@next-mobx-store/type';
-import TodoStore from './TodoStore';
+import TodoStore from "./TodoStore";
 
 export default class RootStore {
-	todoStore: TodoStore;
-	constructor() {
-		this.todoStore = new TodoStore();
-	}
+  todoStore: TodoStore;
+  constructor() {
+    this.todoStore = new TodoStore();
+  }
 }
--- packages/next-mobx-store-core/src/__test__/mocks/TodoStore.ts
+++ packages/next-mobx-store-core/src/__test__/mocks/TodoStore.ts
@@ -1,29 +1,29 @@
-import { DesrializedStore, HydrationStore, IHydrationStore } from '@next-mobx-store/type';
-import { makeAutoObservable } from 'mobx';
-import Todo from './model/Todo';
+import { makeAutoObservable } from "mobx";
+import Todo from "./model/Todo";
+import type { DesrializedStore, IHydrationStore } from "@next-mobx-store/type";
 
 export default class TodoStore implements IHydrationStore {
-	private _todoList: Todo[] = [];
+  private _todoList: Todo[] = [];
 
-	constructor() {
-		makeAutoObservable(this);
-	}
-	addTodo(todo: Todo) {
-		this._todoList.push(todo);
-	}
+  constructor() {
+    makeAutoObservable(this);
+  }
+  addTodo(todo: Todo) {
+    this._todoList.push(todo);
+  }
 
-	removeTodo(id: number) {
-		this._todoList = this._todoList.filter((todo) => todo.id !== id);
-	}
+  removeTodo(id: number) {
+    this._todoList = this._todoList.filter((todo) => todo.id !== id);
+  }
 
-	get todoList() {
-		return this._todoList;
-	}
+  get todoList() {
+    return this._todoList;
+  }
 
-	hydrate(hydrateData?: DesrializedStore<TodoStore>) {
-		if (!hydrateData) return;
-		if (hydrateData.todoList) {
-			this._todoList = hydrateData.todoList;
-		}
-	}
+  hydrate(hydrateData?: DesrializedStore<TodoStore>) {
+    if (!hydrateData) return;
+    if (hydrateData.todoList) {
+      this._todoList = hydrateData.todoList;
+    }
+  }
 }
