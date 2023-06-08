563061b5280c6b5158e106562ebec4a54aa7dae1
Kyeongoh-Ki <56759587+casentino@users.noreply.github.com>
2023-06-08 16:12:41 +0900

    Configure: v0.0.4 (#24)
    * tsup 빌드 설정 수정
    
    * changeset patch
    
    * v0.0.3
    
    * npm ignore 각 패키지에 적용
    resolutions 으로 @types/react 버전 명시

--- apps/docs/src/pages/_app.tsx
+++ apps/docs/src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { RootStoreProvider } from "@next-mobx-store/core";
+import { useHydrateProps } from "@next-mobx-store/hooks";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { store, ...props } = useHydrateProps(pageProps);
+  return (
+    <RootStoreProvider store={store}>
+      <Component {...props} />
+    </RootStoreProvider>
+  );
 }
