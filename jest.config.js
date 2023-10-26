74c4eef78feaf042091cc74c8685ed60b48b6220
casentino <opterk@gmail.com>
2023-10-27 00:55:08 +0900

    package 별로 jest 설정하도록 구성

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,14 @@
-/** @type {import("ts-jest").JestConfigWithTsJest} */
 const { defaults: tsjPreset } = require('ts-jest/presets');
-module.exports = {
-	preset: 'ts-jest/presets/default-esm',
-  testEnvironment: 'node',
-  rootDir: './',    
-  collectCoverage: true,
-  coverageDirectory: '<rootDir>/coverage',
-  testPathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/.yarn'],
-  moduleFileExtensions: ['js','ts', 'tsx'],
-  coverageReporters: ['json', 'html'],
-	transform: {
+/** @type {import("ts-jest").JestConfigWithTsJest} */
+module.exports = { 
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'jsdom',
+  rootDir: '.',
+  moduleDirectories: ['.yarn', '<rootDir>/'],  
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleFileExtensions: ['js','ts', 'tsx'],  
+  transform: {
     ...tsjPreset.transform,
-    // '^.+\\.jsx?$': "../../wrapper.js",
      "^.+\\.[t|j]sx?$": "babel-jest"
- }, 
-  projects: [
-    {
-      testMatch: [
-        './**/*.test.ts'
-      ],
-      testPathIgnorePatterns: ['./dist', '.turbo']
-    }
-  ]
+  }
 };
