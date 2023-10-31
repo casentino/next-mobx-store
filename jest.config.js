50d25d641a5f8576d2f1ca042e7c516e19fc00a9
Kyeongoh-Ki <56759587+casentino@users.noreply.github.com>
2023-10-31 16:04:41 +0900

    Release: v0.3.0 (#55)
    * package 별로 jest 설정하도록 구성 (#47)
    
    * Feature: Readme 수정 (#48)
    
    * Readme 수정
    
    * module exports 방식 수정
    
    * HydrationLocalStoreType 제거 (#49)
    
    * Feature: hooks 테스트작성 (#50)
    
    * useLocalHydrateStore 제거
    
    * test 코드 작성
    
    * version update (#51)
    
    * changesets release 스크립트 수정
    
    * Update CHANGELOG
    
    * version 0.3.0 (#52)
    
    * workflow nodeLinker=node-modules 로 수정 (#54)

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,14 @@
-const { defaults: tsjPreset } = require('ts-jest/presets');
+const { defaults: tsjPreset } = require("ts-jest/presets");
 /** @type {import("ts-jest").JestConfigWithTsJest} */
-module.exports = { 
-  preset: 'ts-jest/presets/default-esm',
-  testEnvironment: 'jsdom',
-  rootDir: '.',
-  moduleDirectories: ['.yarn', '<rootDir>/'],  
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleFileExtensions: ['js','ts', 'tsx'],  
+module.exports = {
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "jsdom",
+  rootDir: ".",
+  moduleDirectories: ["node_modules", "<rootDir>/", ".yarn"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleFileExtensions: ["js", "ts", "tsx"],
   transform: {
     ...tsjPreset.transform,
-     "^.+\\.[t|j]sx?$": "babel-jest"
-  }
+    "^.+\\.[t|j]sx?$": "babel-jest",
+  },
 };
--- packages/next-mobx-store-hooks/jest.config.js
+++ packages/next-mobx-store-hooks/jest.config.js
@@ -1,9 +1,8 @@
-
 /** @type {import("ts-jest").JestConfigWithTsJest} */
 module.exports = {
-  ...require('../../jest.config.js'),
-  displayName: '@next-mobx-store/hooks',
-  testMatch: ['./**/*.test.(ts|tsx)'],
-  moduleDirectories: ['../../.yarn', '../../'],  
-  testPathIgnorePatterns: ['./dist', './.turbo'],
-}
+  ...require("../../jest.config.js"),
+  displayName: "@next-mobx-store/hooks",
+  testMatch: ["./**/*.test.(ts|tsx)"],
+  moduleDirectories: ["../../node_modules", __dirname, "../../.yarn"],
+  testPathIgnorePatterns: ["./dist", "./.turbo"],
+};
