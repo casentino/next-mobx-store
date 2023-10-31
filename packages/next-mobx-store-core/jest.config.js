bd8b8279c61167d5b83d94f034c11982ea50183b
Kyeongoh-Ki <56759587+casentino@users.noreply.github.com>
2023-10-31 16:01:26 +0900

    workflow nodeLinker=node-modules 로 수정 (#54)

--- packages/next-mobx-store-core/jest.config.js
+++ packages/next-mobx-store-core/jest.config.js
@@ -1,12 +1,10 @@
-
 /** @type {import("ts-jest").JestConfigWithTsJest} */
 module.exports = {
-  ...require('../../jest.config.js'),
-  testEnvironment: 'node',
-  displayName: '@next-mobx-store/core',
-  testMatch: ['./**/*.test.(ts|tsx)'],
-  moduleDirectories: ['../../.yarn', '../../'],  
-  testPathIgnorePatterns: ['./dist', './.turbo'],
+  ...require("../../jest.config.js"),
+  testEnvironment: "node",
+  displayName: "@next-mobx-store/core",
+  testMatch: ["./**/*.test.(ts|tsx)"],
+  moduleDirectories: ["../../node_modules", __dirname, "../../.yarn"],
+  testPathIgnorePatterns: ["./dist", "./.turbo"],
   setupFilesAfterEnv: null,
-
-}
+};
